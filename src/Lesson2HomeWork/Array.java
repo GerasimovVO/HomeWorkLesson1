91a6bac8a69eca40e2cdacf54850845337e2958e
Vsevolod <gerasimov-seva@yandex.ru>
2020-10-22 13:08:19 +0700

    Домашняя работа за 3 урок

--- src/Lesson2HomeWork/Array.java
+++ src/Lesson2HomeWork/Array.java
@@ -1,8 +1,11 @@
 package Lesson2HomeWork;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Array {
+
     public static void main(String[] args) {
         int[] a = new int[10];
         for (int i = 0; i < a.length; i++) {
---
+++ src/Lesson3HomeWork/GuessingGame.java
@@ -0,0 +1,48 @@
+package Lesson3HomeWork;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GuessingGame {
+    public static void main(String[] args) throws IOException  {
+
+        int randomNumber = (int) (Math.random() * 10);
+        guessTheNumber(randomNumber);
+
+    }
+    public static void guessTheNumber (int randomNumber) throws IOException{
+
+        System.out.println("Угадайте число от 0 до 9");
+        for (int counter = 1 ; counter < 4; counter++) {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите число: ");
+            int number = Integer.parseInt(reader.readLine());
+            if (number == randomNumber){
+                System.out.println("Поздравляю Вы угадали число с " + counter + " попытки");
+            }
+            else if (number > randomNumber){
+                System.out.println("Загаданное число меньше");
+                int remains = 3 - counter;
+                if (remains == 2){
+                    System.out.println("Осталось две попытки");
+
+                }
+                else System.out.println("Осталась одна попытка");
+            }
+            else {
+                System.out.println("Загаданное число больше");
+                int remains = 3 - counter;
+                if (remains == 2){
+                    System.out.println("Осталось две попытки");
+
+                }
+                else System.out.println("Осталась одна попытка");
+
+            }
+
+
+        }
+
+    }
+}
